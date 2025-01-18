a9c5c97931c0c163af13176d58aa57f13895a599
AriHenrique <arihenriquedev@hotmail.com>
2025-01-18 20:31:36 -0300

    Ajuste para fechamento do programa

--- front/bot/app.py
+++ front/bot/app.py
@@ -8,12 +8,12 @@ import signal
 import psutil
 import threading
 
-
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Morador.db'
 db = SQLAlchemy(app)
 is_whatsapp_connected = False
+
 
 # Modelo de dados atualizado (sem o campo 'nome')
 class Morador(db.Model):
@@ -22,9 +22,11 @@ class Morador(db.Model):
     apartamento = db.Column(db.String(50), nullable=False)
     contato = db.Column(db.String(100), nullable=False)
 
+
 # Criação do banco de dados
 with app.app_context():
     db.create_all()
+
 
 def update_whatsapp_status():
     global is_whatsapp_connected
@@ -36,10 +38,12 @@ def update_whatsapp_status():
     except requests.exceptions.RequestException:
         is_whatsapp_connected = False
 
+
 @app.route('/whatsapp-status', methods=['GET'])
 def whatsapp_status():
     update_whatsapp_status()
     return {'connected': is_whatsapp_connected}
+
 
 # ========= Rota Principal ===========
 @app.route('/')
@@ -48,6 +52,7 @@ def home():
         return redirect(url_for('mostrar_qr'))
     else:
         return redirect(url_for('notificacao'))
+
 
 @app.route('/mostrar_qr')
 def mostrar_qr():
@@ -67,10 +72,13 @@ def mostrar_qr():
 def cadastro():
     blocos_predefinidos = [f'Bloco {i}' for i in range(1, 10)]
     apartamentos_predefinidos = [f"Apartamento {i}0{j}" for i in range(1, 6) for j in range(1, 5)]
-    return render_template('cadastro.html', blocos=blocos_predefinidos, apartamentos=apartamentos_predefinidos, conectado=is_whatsapp_connected)
+    return render_template('cadastro.html', blocos=blocos_predefinidos, apartamentos=apartamentos_predefinidos,
+                           conectado=is_whatsapp_connected)
+
 
 @app.route('/adicionar', methods=['POST'])
 def adicionar_morador():
+    global contato, contatos
     if request.is_json:
         data = request.get_json()
         bloco = data.get('bloco')
@@ -124,7 +132,9 @@ def remover_morador():
         flash(f'Todos os contatos do {bloco} - {apartamento} foram removidos com sucesso.')
         return redirect(url_for('remover_morador'))
 
-    return render_template('remover.html', blocos=blocos_predefinidos, apartamentos=apartamentos_predefinidos,conectado=is_whatsapp_connected)
+    return render_template('remover.html', blocos=blocos_predefinidos, apartamentos=apartamentos_predefinidos,
+                           conectado=is_whatsapp_connected)
+
 
 # ========= Rota para Modificar Contatos ===========
 @app.route('/modificar', methods=['GET', 'POST'])
@@ -151,7 +161,6 @@ def modificar_contatos():
     )
 
 
-
 @app.route('/atualizar_contatos', methods=['POST'])
 def atualizar_contatos():
     bloco = request.form.get('bloco')
@@ -171,12 +180,15 @@ def atualizar_contatos():
     flash(f"Contatos atualizados com sucesso para {bloco} - {apartamento}.")
     return redirect(url_for('modificar_contatos'))
 
+
 # ========= Rota para Enviar Notificação ===========
 @app.route('/notificacao')
 def notificacao():
     blocos_predefinidos = [f'Bloco {i}' for i in range(1, 10)]
     apartamentos_predefinidos = [f"Apartamento {i}0{j}" for i in range(1, 6) for j in range(1, 5)]
-    return render_template('notificacao.html', blocos=blocos_predefinidos, apartamentos=apartamentos_predefinidos, conectado=is_whatsapp_connected)
+    return render_template('notificacao.html', blocos=blocos_predefinidos, apartamentos=apartamentos_predefinidos,
+                           conectado=is_whatsapp_connected)
+
 
 @app.route('/enviar_notificacao', methods=['POST'])
 def enviar_notificacao():
@@ -211,20 +223,19 @@ _Portaria_"""
             return redirect(url_for('mostrar_qr'))
     return redirect(url_for('notificacao'))
 
+
 @app.route('/fechar')
 def fechar_sistema():
     try:
-        import requests
         requests.get('http://localhost:3000/encerrar')
-        current_pid = os.getpid()
-        parent = psutil.Process(current_pid)
-        for child in parent.children(recursive=True):
-            child.terminate()
-        parent.terminate()
-        flash('Sistema encerrado com sucesso.')
-    except Exception as e:
-        flash(f'Erro ao encerrar o sistema: {str(e)}')
-    return redirect(url_for('index'))
+    except Exception:
+        pass
+    current_pid = os.getpid()
+    parent = psutil.Process(current_pid)
+    for child in parent.children(recursive=True):
+        child.terminate()
+    os.kill(current_pid, signal.SIGTERM)
+    return "Sistema encerrado com sucesso."
 
 
 # ========= Iniciar o Servidor ===========
@@ -233,4 +244,4 @@ if __name__ == '__main__':
     if caminho_pasta.exists() and caminho_pasta.is_dir():
         status_thread = threading.Thread(target=update_whatsapp_status, daemon=True)
         status_thread.start()
-    app.run(debug=True)
+    app.run()
